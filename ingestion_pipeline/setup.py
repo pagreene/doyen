08360bf04d8251d1b042f33dfd9227ba7a627ce7
Patrick Greene <patrick.anton.greene@gmail.com>
2023-04-20 23:34:30 -0400

    Get setup.py working.

--- ingestion_pipeline/setup.py
+++ ingestion_pipeline/setup.py
@@ -1,35 +1,24 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='doyen_ingestion',
-    version='0.1',
+    name="doyen_ingestion",
+    version="0.1",
     packages=find_packages(),
     include_package_data=True,
-    entry_points={
-        'console_scripts': [
-            'doyen-ingest = doyen.pubmed_processor:main'
-        ]
-    },
-    python_requires='>=3.9',
+    entry_points={"console_scripts": ["doyen-ingest = doyen.pubmed_processor:main"]},
+    python_requires=">=3.9",
     install_requires=[
-        'elasticsearch',
-        'git+https://github.com/pagreene/indra.git@get-citations#egg=indra'
-        'pandas',
-        'scikit-learn'
+        "elasticsearch",
+        "indra @ https://github.com/pagreene/indra/archive/get-citations.zip",
     ],
-    extras_require={
-        'dev': [
-            'pytest',
-            'black'
-        ]
-    },
+    extras_require={"dev": ["pytest", "black"]},
     classifiers=[
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.9',
-        'Operating System :: OS Independent',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX :: Linux',
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
     ],
 )

f5b870e6d9469ae4a0a4233f45a712e3e8584cb5
Patrick Greene <patrick.anton.greene@gmail.com>
2023-04-29 15:12:42 -0400

    Run black on setup.py.

--- ingestion_pipeline/setup.py
+++ ingestion_pipeline/setup.py
@@ -6,7 +6,11 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     package_data={"doyen_ingestion": ["resources/*"]},
-    entry_points={"console_scripts": ["doyen-ingest = doyen_ingestion.pubmed_processor:fill_elasticsearch"]},
+    entry_points={
+        "console_scripts": [
+            "doyen-ingest = doyen_ingestion.pubmed_processor:fill_elasticsearch"
+        ]
+    },
     python_requires=">=3.7",
     install_requires=[
         "click",

d108f97b6630bc99071a23f898d27588f5454ff8
Patrick Greene <patrick.anton.greene@gmail.com>
2023-04-29 22:04:06 -0400

    Micro-increment version.

--- ingestion_pipeline/setup.py
+++ ingestion_pipeline/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="doyen_ingestion",
-    version="1.0",
+    version="1.0.1",
     packages=find_packages(),
     include_package_data=True,
     package_data={"doyen_ingestion": ["resources/*"]},
